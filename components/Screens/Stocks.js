41be05cb26304e41cfa70376ab87bf3a93d65f64
Zhang Zhikai <zhangzhikai71@gmail.com>
2021-07-02 20:49:47 +0800

    Using StockAPIFunctions instead

--- components/Screens/Stocks.js
+++ components/Screens/Stocks.js
@@ -8,6 +8,8 @@ import {StatusBar} from 'expo-status-bar';
 import FlatButton from '../Buttons/button';
 import MinusButton from '../Buttons/negativeButton';
 import PlusButton from '../Buttons/positiveButton';
+import getCompanyName from '../StockAPIFunctions/getCompanyName';
+import getCurrentPrice from '../StockAPIFunctions/getCurrentPrice';
 import * as firebase from 'firebase';
 
 
@@ -38,8 +40,10 @@ export default function Stocks({navigation}) {
     const [NumShares, setNumShares] = useState(0);
     const [Ticker, setTicker] = useState('');
     const [isLoading, setLoading] = useState(true);
-
-    const getPriceOfTicker = (ticker) => {
+  
+   // cannot move this out will cause app to have error 
+   // likely to mean cannot move anything that connect to firestore docs out
+   const getPriceOfTicker = (ticker) => {
         //let price = stockPriceDoc()
         const stockPriceDoc = () => stockCollection
             .doc(ticker)
@@ -74,7 +78,8 @@ export default function Stocks({navigation}) {
     
       if (checkTicker) {
       
-         let prevHoldings = await getNumSharesOfTicker(ticker);
+         let prevHoldings = await  getNumSharesOfTicker(ticker);
+        // getNumOfShares(ticker) //
          if (NumShares > prevHoldings) {
            TickerFailedAlert("Number of Shares sold more than owned")
          } else if (NumShares == prevHoldings) {
@@ -147,6 +152,7 @@ export default function Stocks({navigation}) {
             // await functions return first to get prevPrice and prevHoldings
             let prevPrice = await getPriceOfTicker(ticker);
             let prevHoldings = await getNumSharesOfTicker(ticker);
+            console.log("updating stock prev number: " + prevHoldings)
             // console.log("UPDATE:" + checkTicker)
             console.log("Adding to existing")
             const stockDoc = stockCollection
@@ -179,7 +185,8 @@ export default function Stocks({navigation}) {
             
             
             // in the end need to include MIC when we support other exchanges
-           let nameOfCompany = await getName(ticker)
+           // let nameOfCompany = await getName(ticker)
+           let nameOfCompany = await getCompanyName(ticker)
            console.log("noc" + (nameOfCompany))
            console.log("Testing Orhad")
 
@@ -248,7 +255,7 @@ export default function Stocks({navigation}) {
           
           querySnapshot.forEach(documentSnapshot => {
             
-            getCurrPrice(documentSnapshot.id).then((result) => {
+            getCurrentPrice(documentSnapshot.id).then((result) => {
               stockData.push({x: documentSnapshot.data().Name, y: result * documentSnapshot.data().Shares})
               stocks.push({
               ...documentSnapshot.data(), // provides the old price and old num Shares
@@ -260,96 +267,27 @@ export default function Stocks({navigation}) {
               perChange: (((result - documentSnapshot.data().Price) / documentSnapshot.data().Price) * 100).toFixed(2)
             })
 
-           
-            // console.log(stocks[stocks.length - 1].key)
             stockValue = stockValue + stocks[stocks.length - 1].currValue
-         
-            // console.log("Curr price result: " + result); => defaulted to return 150 for calculation
-            
             setTotalValue(stockValue);
-            // console.log(stockValue)
-            // setLoading(false)
+         
             })
-            // console.log("Stock Data Array : "  + stockData[1].x)
             setStockList(stocks);
             setLoading(false)
-            //console.log("Updating Stock value on Home page")
-            
-            
-            
-          
+
           });
-          
-          // update here how many times does it access
-          
+
         })
         
       return () => subscriber();
     }, []);
-    
-
-    
-    
     
     const axios = require('axios');
     const params = {
       access_key: '34c968023042747b40e1af689bf81751'
        // access_key: '0'
     }
+   
 
-   
-    function getCurrPrice(ticker) {
-      
-      // Obtain the latest end-of-day data for a given stock symbol. Example: /tickers/AAPL/eod/latest
-      const url = `http://api.marketstack.com/v1/eod/latest?access_key=${params.access_key}&symbols=${ticker}`
-      // output 1 day
-      // `http://api.marketstack.com/v1/eod?access_key=${params.access_key}&symbols=${ticker}`
-      // output 100 day page limit
-      
-      const quote = async () => axios.get(url)
-        .then(response => {
-          const apiResponse = response.data;
-          if (Array.isArray(apiResponse['data'])) {
-            console.log(apiResponse['data'][0]['close'])
-            return apiResponse['data'][0]['close']
-          }
-        }).catch(error => {
-          // return 150;
-          console.log(error.response.data);
-        });
-        
-        
-        return quote();
-        
-    }
-    //either return name or catch error and return that this does not exist, NA
-    function getName(ticker) {
-      
-      
-      const url = `http://api.marketstack.com/v1/tickers/${ticker}?access_key=${params.access_key}`
-      
-      const quote = async () => axios.get(url)
-        .then(response => {
-          console.log("Then Reached")
-          const apiResponse = response.data;
-          console.log(apiResponse);
-          console.log(Array.isArray(apiResponse['data']))
-        
-          return apiResponse['name'];
-          
-        }).catch(error => {
-          console.log(error);
-          return 'NA'
-          // return 'Apple';
-          //return 'Tesla';
-          // return 'Disney'
-        });
-        
-        
-        return quote();
-        
-    }
-   
   updateTotalStockValue();
 
    const [data, setData] = useState({
@@ -399,14 +337,6 @@ export default function Stocks({navigation}) {
      // console.log("val == null:" + (val == null))
      // omitted the fullstop
      // does not replace period . 
-     /*console.log(typeof(parseFloat(val)) + " :  " + parseFloat(val));
-     if (typeof(parseFloat(val)) != "number") {
-      Alert.alert("Not a valid number!", [{text: 'Okay'}])
-      setData({
-        ...data,
-        isValidNumberInput: false,
-      })
-     }*/
      // given input is 1.5.5, parsefloat and setdata will make it 1.5
      let value = parseFloat(val.replace(/[- #*;,<>\{\}\[\]\\\/]/gi, ''))
 
